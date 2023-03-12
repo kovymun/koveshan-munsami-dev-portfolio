4a6db57fb06e9cedca031b7685e5d6d42245bdda
Koveshan <codingstudent2021@gmail.com>
2023-03-13 01:46:42 +0200

    Adding Analytics script

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,29 @@
 
 import "../styles/styles.css";
 import "../styles/globals.css";
+import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-G1C4FVXEZP"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-G1C4FVXEZP', {
+              page_path: window.location.pathname,});
+            `,
+        }}
+      />
+      <Component {...pageProps} />
+    </div>
+  );
 }
